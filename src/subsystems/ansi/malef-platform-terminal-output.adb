aa92fd8321dd6f3882b26503844512c13bced550
Santiago Ramírez <ramirez.sa94@gmail.com>
2024-03-10 12:36:20 -0300

    Fix typo for escape code and add comments.

--- src/subsystems/ansi/malef-platform-terminal-output.adb
+++ src/subsystems/ansi/malef-platform-terminal-output.adb
@@ -197,7 +197,7 @@ package body Malef.Platform.Terminal.Output is
       Format (7, 0, [others => False]);
       Move_To (1, 1);
       Opened_Frames := 0;
-      Buffer.Put (ASCII.ESC & "[25l");
+      Buffer.Put (ASCII.ESC & "[?25l"); -- Make cursor invisible
       Flush;
    end Initialize;
 
@@ -206,7 +206,7 @@ package body Malef.Platform.Terminal.Output is
       -- TODO: Call termios
       Buffer.Put (ASCII.ESC & "[0m"     -- Clear format
                 & ASCII.ESC & "[?12h"   -- Restore terminal
-                & ASCII.ESC & "[?25h");
+                & ASCII.ESC & "[?25h"); -- Make cursor visible
       Flush;
    end Finalize;
 
